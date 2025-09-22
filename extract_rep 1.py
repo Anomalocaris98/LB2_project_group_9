bd017bb1043f5e3bf9ea1d2177634103da4e0fae
Anomalocaris98 <anomalocaris@LAPTOP-B20L6OCT.localdomain>
2025-09-22 22:10:53 +0200

    python script used to extract representatives proteins from .tsv files

---
+++ extract_rep 1.py
@@ -0,0 +1,22 @@
+file_input=input("Insert the filename: ")
+file_output=input("Insert the output: ")
+
+def extract_rep (file_input,file_output):
+    list_rep = []
+    list_rep_cleaned=[]
+    with open(file_input, "r") as tsv:
+        for line in tsv:
+            columns = line.split()
+            list_rep.append(columns[0])
+        for seq in list_rep:
+            if seq not in list_rep_cleaned:
+                list_rep_cleaned.append(seq)
+    with open(file_output,"w") as output:
+        for element in list_rep_cleaned:
+            output.write(element + "\n")
+            
+
+    return list_rep_cleaned
+
+print(extract_rep(file_input,file_output))
+
