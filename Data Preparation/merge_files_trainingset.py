63e9e9fa49a3606fcc077da3306eeca6241c6b1d
Anomalocaris98 <anomalocaris@LAPTOP-B20L6OCT.localdomain>
2025-12-11 12:25:05 +0100

    added plots

--- Data Preparation/merge_files_trainingset.py
+++ Data Preparation/merge_files_trainingset.py
@@ -1,17 +1,17 @@
 def unisci_file(lista_file, file_output):
     """
-    Unisce il contenuto di più file di testo in un unico file.
-    
-    Parametri:
-    - lista_file: lista di nomi dei file da unire
-    - file_output: nome del file finale unito
+    Unisce il contenuto di più file TSV con lo stesso header
+    in un unico file, scrivendo l'header solo una volta.
     """
+    first = True
     with open(file_output, "w", encoding="utf-8") as out:
         for nome in lista_file:
             with open(nome, "r", encoding="utf-8") as f:
-                contenuto = f.read()
-                out.write(contenuto + "\n")  # aggiungo anche una riga vuota tra i file
-
-# esempio di utilizzo
-file_da_unire = ['subset1_arricchito.tsv','subset2_arricchito.tsv','subset3_arricchito.tsv','subset4_arricchito.tsv','subset5_arricchito.tsv']
-unisci_file(file_da_unire, "training_file_all_seq.tsv")
+                lines = f.readlines()
+                if first:
+                    # Scrivo anche l'header del primo file
+                    out.writelines(lines)
+                    first = False
+                else:
+                    # Dal secondo file in poi salto la prima riga (header)
+                    out.writelines(lines[1:])
